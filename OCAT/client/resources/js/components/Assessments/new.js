25b7dc3f5bcb4fccb73d4d4eb0286b7d941d8322
Risha0205 <choubera@mail.uc.edu>
2021-03-09 14:31:38 -0500

    Learning and added style.css

--- OCAT/client/resources/js/components/Assessments/new.js
+++ OCAT/client/resources/js/components/Assessments/new.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import { AssessmentService } from '../shared/services/assessment.service';
 import { useForm } from "react-hook-form";
-
+import './styles.css'
 export function AssessmentNew() {
 
   //create a form that utilizes the "onSubmit" function to send data to OCAT/client/libs and onto the OCAT/server/routes express API
   const onSubmit = async (data) => {
     await AssessmentService.submit(data);
   };
-  const { register, handleSubmit } = useForm();
+
+  const { register, handleSubmit, errors } = useForm();
+  const spanStyles = {
+    color: "green",
+
+  };
 
   return (
-    <form >
+    <form style={{ backgroundColor: 'transparent', height: 200, flexdirection: 'column' }}>
 
-      <label style={{ color: 'red' }}>First Name</label>
-      <input name='Risha' />
+      {/* <label style={spanStyles}>First Name</label>
+      <input name='Risha' /> */}
 
-      <label style={{ color: 'red' }}>Last Name</label>
-      <input name='Risha' />
+      <div className="form-control">
+        <label>First Name</label>
+        <input type="text" name="first_name" ref={register} />
+      </div>
+
+      <div className="form-control">
+        <label>Last Name</label>
+        <input type="text" name="last_name" ref={register} />
+      </div>
 
 
       <select name="gender" ref={register}>
@@ -28,5 +40,26 @@ export function AssessmentNew() {
       <button type="submit">Submit</button>
 
     </form>
+
+
+
   );
 }
+
+
+{/* <div className="App">
+<form onSubmit={handleSubmit(onSubmit)}>
+  <div className="form-control">
+    <label>Email</label>
+    <input type="text" name="email" ref={register} />
+  </div>
+  <div className="form-control">
+    <label>Password</label>
+    <input type="password" name="password" ref={register} />
+  </div>
+  <div className="form-control">
+    <label></label>
+    <button type="submit">Login</button>
+  </div>
+</form>
+</div> */}
