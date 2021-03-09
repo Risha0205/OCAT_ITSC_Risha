26b247273be18918005af986abf72e08cc1bd4b2
Risha0205 <choubera@mail.uc.edu>
2021-03-09 14:33:31 -0500

    removed commented code

--- OCAT/client/resources/js/components/Assessments/new.js
+++ OCAT/client/resources/js/components/Assessments/new.js
@@ -46,20 +46,3 @@ export function AssessmentNew() {
   );
 }
 
-
-{/* <div className="App">
-<form onSubmit={handleSubmit(onSubmit)}>
-  <div className="form-control">
-    <label>Email</label>
-    <input type="text" name="email" ref={register} />
-  </div>
-  <div className="form-control">
-    <label>Password</label>
-    <input type="password" name="password" ref={register} />
-  </div>
-  <div className="form-control">
-    <label></label>
-    <button type="submit">Login</button>
-  </div>
-</form>
-</div> */}
