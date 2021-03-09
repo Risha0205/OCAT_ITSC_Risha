8a398276f400c021a7373342deb4b6265aa27ba4
Risha0205 <choubera@mail.uc.edu>
2021-03-08 19:29:54 -0500

    Started form

--- OCAT/client/resources/js/components/Assessments/new.js
+++ OCAT/client/resources/js/components/Assessments/new.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { AssessmentService } from '../shared/services/assessment.service';
+import { useForm } from "react-hook-form";
 
-export function AssessmentNew(){
+export function AssessmentNew() {
 
-    //create a form that utilizes the "onSubmit" function to send data to OCAT/client/libs and onto the OCAT/server/routes express API
-    const onSubmit = async (data) => {
-        await AssessmentService.submit(data);
-    };
+  //create a form that utilizes the "onSubmit" function to send data to OCAT/client/libs and onto the OCAT/server/routes express API
+  const onSubmit = async (data) => {
+    await AssessmentService.submit(data);
+  };
+  const { register, handleSubmit } = useForm();
 
   return (
-    <form>
-        <button type="submit">Submit</button>
+    <form >
+
+      <label style={{ color: 'red' }}>First Name</label>
+      <input name='Risha' />
+
+      <label style={{ color: 'red' }}>Last Name</label>
+      <input name='Risha' />
+
+
+      <select name="gender" ref={register}>
+        <option value="female">female</option>
+        <option value="male">male</option>
+        <option value="other">other</option>
+      </select>
+      <button type="submit">Submit</button>
+
     </form>
   );
 }
