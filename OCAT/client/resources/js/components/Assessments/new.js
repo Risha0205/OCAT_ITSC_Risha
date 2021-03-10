5bff3f035a64c937e726288021436b900852f28c
Risha0205 <choubera@mail.uc.edu>
2021-03-10 15:32:47 -0500

    added datepicker

--- OCAT/client/resources/js/components/Assessments/new.js
+++ OCAT/client/resources/js/components/Assessments/new.js
@@ -1,48 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AssessmentService } from '../shared/services/assessment.service';
 import { useForm } from "react-hook-form";
-import './styles.css'
+import DatePicker from 'react-datepicker';
 export function AssessmentNew() {
 
   //create a form that utilizes the "onSubmit" function to send data to OCAT/client/libs and onto the OCAT/server/routes express API
   const onSubmit = async (data) => {
     await AssessmentService.submit(data);
   };
-
-  const { register, handleSubmit, errors } = useForm();
-  const spanStyles = {
-    color: "green",
-
-  };
+  const { register, handleSubmit } = useForm();
+  const [date, setDate] = useState(new Date())
+  // const spanStyles = {
+  //   color: "green",
+  // };
+  const handleChange = date => setDate(date);
 
   return (
     <form style={{ backgroundColor: 'transparent', height: 200, flexdirection: 'column' }}>
 
-      {/* <label style={spanStyles}>First Name</label>
-      <input name='Risha' /> */}
+    {/* <label style={spanStyles}>First Name</label>
+    <input name='Risha' /> */}
 
-      <div className="form-control">
-        <label>First Name</label>
-        <input type="text" name="first_name" ref={register} />
-      </div>
+    <div className="form-control">
+      <label>First Name</label><br/>
+      <input type="text" name="first_name" ref={register} />
+    </div>
 
-      <div className="form-control">
-        <label>Last Name</label>
-        <input type="text" name="last_name" ref={register} />
-      </div>
+    <div className="form-control">
+      <label>Last Name</label><br/>
+      <input type="text" name="last_name" ref={register} />
+    </div>
 
-
-      <select name="gender" ref={register}>
-        <option value="female">female</option>
-        <option value="male">male</option>
-        <option value="other">other</option>
-      </select>
-      <button type="submit">Submit</button>
-
-    </form>
+    <div className="form-control">
+      <label>Date of Birth</label>
+      <DatePicker selected={date} onChange={handleChange} />
+    </div>
 
 
+    <select name="gender" ref={register}>
+      <option value="female">female</option>
+      <option value="male">male</option>
+      <option value="other">other</option>
+    </select>
+    <button type="submit">Submit</button>
 
+  </form>
   );
 }
-
